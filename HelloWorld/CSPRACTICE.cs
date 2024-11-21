a1e27eb355b22f163fe9e559c56b593e0493db25
GaiaDweller <119241181+GaiaDweller@users.noreply.github.com>
2024-11-21 10:17:26 -0700

    Goofed around
    blehhhhhhhh

--- HelloWorld/CSPRACTICE.cs
+++ HelloWorld/CSPRACTICE.cs
@@ -2,11 +2,25 @@
 
 namespace HelloWorld
 {
-  class Program
+  class secretworddata
   {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World!");    
-    }
+    public string Word { get; set;}
+  }
+  class Vroom 
+  {
+    int speed = 1;
+    string word = "Vroom";
+    char character = 'C';
+      static void Main(string[] args)
+      {
+        Vroom MyVroomVroom = new Vroom(); 
+        Console.WriteLine(MyVroomVroom.word);
+        Console.WriteLine(MyVroomVroom.character);
+        Console.WriteLine(MyVroomVroom.speed);
+        Console.WriteLine("SAY SOMETHING COOL MY BROTHER:");
+        string mysupercoolinput = Console.ReadLine();
+        Console.WriteLine("THATS SO REAL MY FRIEND");
+        Console.WriteLine($"Random ahh Dude: {mysupercoolinput}");
+      }
   }
 }
--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1eb2d748e53f3641bc14bb294e894bb6d99d8c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
